493de54701180cabf68567285f93aa4a19fff2fd
Russ Hyde <russ@jumpingrivers.com>
2022-11-02 10:52:53 +0000

    refac: extract data-formatting code from plot_cluster_data

--- R/plot_cluster_sina.R
+++ R/plot_cluster_sina.R
@@ -17,34 +17,12 @@ plot_cluster_sina <- function(pldf,
                               varx = "logistic_growth_rate",
                               mut_regexp = "S:A222V",
                               lineage_regexp = NULL) {
-  # ~ 	pldf = pl$data
-  sc1 <- pldf[pldf$isTip, ]
-  sc1$varx <- sc1[[varx]]
-  sc1$colour_var <- ""
-
-  if (!is.null(mut_regexp)) {
-    y <- do.call(cbind, lapply(mut_regexp, function(xre) {
-      ifelse(grepl(sc1$allmuts, pattern = xre), xre, "")
-    }))
-    ymut <- apply(y, 1, function(x) paste(x, collapse = "."))
-  } else {
-    ymut <- rep("", nrow(sc1))
-  }
-
-  if (!is.null(lineage_regexp)) {
-    y <- do.call(cbind, lapply(lineage_regexp, function(xre) {
-      ifelse(grepl(sc1$lineage, pattern = xre), xre, "")
-    }))
-    ylin <- apply(y, 1, function(x) paste(x, collapse = "."))
-  } else {
-    ylin <- rep("", nrow(sc1))
-  }
-
-  sc1$mutation_lineage <- paste(ymut, ylin, sep = "_")
-  fx <- as.factor(sc1$mutation_lineage)
-  sc1$x <- as.numeric(fx)
-  sc1$x <- stats::rnorm(nrow(sc1), sc1$x, .15) ## seed value???
-  sc1$y <- sc1[[varx]]
+  sc1 <- format_cluster_sina_data(
+    pldf,
+    varx = varx,
+    mut_regexp = mut_regexp,
+    lineage_regexp = lineage_regexp
+  )
 
   p1 <- ggplot2::ggplot(sc1) +
     ggiraph::geom_point_interactive(
@@ -76,4 +54,43 @@ plot_cluster_sina <- function(pldf,
   htmlwidgets::saveWidget(g1,
     file = as.character(glue::glue("{output_dir}/sina-{varx}.html"))
   )
+}
+
+#' Reformats data for a tree-plot for presentation in a sina plot
+#'
+#' @inheritParams plot_cluster_sina
+
+format_cluster_sina_data <- function(pldf,
+                                     varx,
+                                     mut_regexp = NULL,
+                                     lineage_regexp = NULL) {
+  sc1 <- pldf[pldf$isTip, ]
+  sc1$varx <- sc1[[varx]]
+  sc1$colour_var <- ""
+
+  if (!is.null(mut_regexp)) {
+    y <- do.call(cbind, lapply(mut_regexp, function(xre) {
+      ifelse(grepl(sc1$allmuts, pattern = xre), xre, "")
+    }))
+    ymut <- apply(y, 1, function(x) paste(x, collapse = "."))
+  } else {
+    ymut <- rep("", nrow(sc1))
+  }
+
+  if (!is.null(lineage_regexp)) {
+    y <- do.call(cbind, lapply(lineage_regexp, function(xre) {
+      ifelse(grepl(sc1$lineage, pattern = xre), xre, "")
+    }))
+    ylin <- apply(y, 1, function(x) paste(x, collapse = "."))
+  } else {
+    ylin <- rep("", nrow(sc1))
+  }
+
+  sc1$mutation_lineage <- paste(ymut, ylin, sep = "_")
+  fx <- as.factor(sc1$mutation_lineage)
+  sc1$x <- as.numeric(fx)
+  sc1$x <- stats::rnorm(nrow(sc1), sc1$x, .15) ## seed value???
+  sc1$y <- sc1[[varx]]
+
+  sc1
 }
