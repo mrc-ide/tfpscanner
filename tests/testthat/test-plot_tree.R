f331dde1201e74590a763cfeeb798a16cb24792a
Russ Hyde <russ@jumpingrivers.com>
2022-11-09 16:08:21 +0000

    test: date is absent from save_trees() filepaths by default

--- tests/testthat/test-plot_tree.R
+++ tests/testthat/test-plot_tree.R
@@ -7,20 +7,50 @@ describe("save_trees", {
   )
   branch_col <- "logistic_growth_rate"
 
-  it("creates an .svg from the 'noninteractive' entry", {
-    td <- withr::local_tempdir(pattern = "noninteractive-svg")
-    output_path <- file.path(td, glue::glue("tree-{branch_col}-{Sys.Date()}.svg"))
+  it("creates an .svg from the 'noninteractive' entry - default: without the current date", {
+    td <- withr::local_tempdir(pattern = "no-date-svg")
+    output_path <- file.path(td, glue::glue("tree-{branch_col}.svg"))
 
-    save_trees(tree_list, branch_col = branch_col, output_dir = td, n_leaves = 100)
+    save_trees(
+      tree_list,
+      branch_col = branch_col, output_dir = td, n_leaves = 100
+    )
 
     expect_true(file.exists(output_path))
   })
 
-  it("creates an .rds from the 'interactive' entry", {
+  it("creates an .rds from the 'interactive' entry - default: without the current date", {
+    td <- withr::local_tempdir(pattern = "no-date-rds")
+    output_path <- file.path(td, glue::glue("tree-{branch_col}.rds"))
+
+    save_trees(
+      tree_list,
+      branch_col = branch_col, output_dir = td, n_leaves = 100
+    )
+
+    expect_true(file.exists(output_path))
+  })
+
+  it("creates an .svg from the 'noninteractive' entry - including the current date", {
+    td <- withr::local_tempdir(pattern = "noninteractive-svg")
+    output_path <- file.path(td, glue::glue("tree-{branch_col}-{Sys.Date()}.svg"))
+
+    save_trees(
+      tree_list,
+      branch_col = branch_col, output_dir = td, n_leaves = 100, include_date = TRUE
+    )
+
+    expect_true(file.exists(output_path))
+  })
+
+  it("creates an .rds from the 'interactive' entry - including the current date", {
     td <- withr::local_tempdir(pattern = "interactive-rds")
     output_path <- file.path(td, glue::glue("tree-{branch_col}-{Sys.Date()}.rds"))
 
-    save_trees(tree_list, branch_col = branch_col, output_dir = td, n_leaves = 100)
+    save_trees(
+      tree_list,
+      branch_col = branch_col, output_dir = td, n_leaves = 100, include_date = TRUE
+    )
 
     expect_true(file.exists(output_path))
   })
